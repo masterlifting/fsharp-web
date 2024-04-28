8235abe2a0dd738ce2dddae7c594a14ffd3da4be
Andrey Pestunov <masterlifting@gmail.com>
2024-04-28 18:26:42 +0200

    Add Http module with get and post functions

--- src/Core.fs
+++ src/Core.fs
@@ -1,1 +1,8 @@
-﻿module Core
+﻿module Web.Core
+
+module Http =
+    let get (url: string) =
+        async { return Error "Http.get not implemented." }
+
+    let post (url: string) (data: byte[]) =
+        async { return Error "Http.post not implemented." }
