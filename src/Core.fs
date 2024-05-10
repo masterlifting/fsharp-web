d57509d8c0ff050e82ae77716e4e492649233b14
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-05-10 20:00:15 +0200

    feat: Add Uri parsing and query parameter extraction to Http module

--- src/Core.fs
+++ src/Core.fs
@@ -1,8 +1,26 @@
 ﻿module Web.Core
 
+open System
+open Infrastructure
+
 module Http =
     let get (url: string) =
         async { return Error "Http.get not implemented." }
 
     let post (url: string) (data: byte[]) =
         async { return Error "Http.post not implemented." }
+
+    let getUri (url: string) =
+        try
+            Ok <| Uri url
+        with ex ->
+            Error ex.Message
+
+    let getQueryParameters (uri: Uri) =
+        uri.Query.Split('&')
+        |> Array.map (fun parameter ->
+            match parameter.Split('=') with
+            | parts when parts.Length = 2 -> Ok(parts.[0], parts.[1])
+            | _ -> Error "Invalid query parameter.")
+        |> DSL.Seq.resultOrError
+        |> Result.map Map
