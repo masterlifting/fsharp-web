726fa7685315bc8828d803e30eeb1eadb5be5784
Andrei Pestunov <masterlifting@gmail.com>
2024-06-01 18:26:30 +0200

    refactor: Refactor Http module to separate Uri parsing and query parameter extraction

--- src/Core.fs
+++ src/Core.fs
@@ -4,23 +4,24 @@ open System
 open Infrastructure
 
 module Http =
-    let get (url: string) =
+    module Mapper =
+        let toUri (url: string) =
+            try
+                Ok <| Uri url
+            with ex ->
+                Error ex.Message
+
+        let toQueryParams (uri: Uri) =
+            uri.Query.Split('&')
+            |> Array.map (fun parameter ->
+                match parameter.Split('=') with
+                | parts when parts.Length = 2 -> Ok(parts.[0], parts.[1])
+                | _ -> Error $"Invalid query parameter of '{parameter}'.")
+            |> DSL.Seq.resultOrError
+            |> Result.map Map
+
+    let get (url: Uri) =
         async { return Error "Http.get not implemented." }
 
-    let post (url: string) (data: byte[]) =
+    let post (url: Uri) (data: byte[]) =
         async { return Error "Http.post not implemented." }
-
-    let getUri (url: string) =
-        try
-            Ok <| Uri url
-        with ex ->
-            Error ex.Message
-
-    let getQueryParameters (uri: Uri) =
-        uri.Query.Split('&')
-        |> Array.map (fun parameter ->
-            match parameter.Split('=') with
-            | parts when parts.Length = 2 -> Ok(parts.[0], parts.[1])
-            | _ -> Error "Invalid query parameter.")
-        |> DSL.Seq.resultOrError
-        |> Result.map Map
