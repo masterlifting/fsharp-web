7155f41f72d6b626b252954ff995bc8c59ffb53e
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-06-06 20:51:48 +0200

    refactor: Remove commented out code in Mapper.fs

--- src/Mapper.fs
+++ src/Mapper.fs
@@ -2,14 +2,14 @@ module Web.Mapper
 
 open Infrastructure.DSL.ActivePatterns
 
-module Bots =
-    module Telegram =
-        let toCoreMessage (message: Domain.External.Bots.Telegram.Text) : Domain.Internal.Bots.Telegram.Text =
-            { Id = message.Id |> Option.ofNullable
-              ChatId = message.ChatId
-              Value = message.Value }
+// module Bots =
+//     module Telegram =
+//         let toCoreMessage (message: Domain.External.Bots.Telegram.Text) : Domain.Internal.Bots.Telegram.Text =
+//             { Id = message.Id |> Option.ofNullable
+//               ChatId = message.ChatId
+//               Value = message.Value }
 
-        let toSourceMessage (message: Domain.Internal.Bots.Telegram.Text) : Domain.External.Bots.Telegram.Text =
-            { Id = message.Id |> Option.toNullable
-              ChatId = message.ChatId
-              Value = message.Value }
+//         let toSourceMessage (message: Domain.Internal.Bots.Telegram.Text) : Domain.External.Bots.Telegram.Text =
+//             { Id = message.Id |> Option.toNullable
+//               ChatId = message.ChatId
+//               Value = message.Value }
