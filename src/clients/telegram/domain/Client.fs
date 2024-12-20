9c54dc85cd34daea5389b37a0e44199e1ae6d80a
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-12-20 22:57:38 +0100

    Refactor ChatId type to add ValueStr property and parse method for improved usability and error handling

--- src/clients/telegram/domain/Client.fs
+++ src/clients/telegram/domain/Client.fs
@@ -2,6 +2,7 @@
 module Web.Telegram.Domain.Client
 
 open System.Collections.Concurrent
+open Infrastructure.Domain
 open Telegram.Bot
 
 type TelegramBot = TelegramBotClient
@@ -17,3 +18,12 @@ type ChatId =
     member this.Value =
         match this with
         | ChatId value -> value
+        
+    member this.ValueStr =
+        this.Value |> string
+        
+    static member parse(value: string) =
+        try
+            value |> int64 |> ChatId |> Ok
+        with
+        | _ -> $"'{value}' for ChatId" |> NotSupported |> Error
