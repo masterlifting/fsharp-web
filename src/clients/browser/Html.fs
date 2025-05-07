4cb9972345de39c8e2ef5e46f85cb2789241f42c
Andrei Pestunov <andrei.pestunov@akvelon.com>
2025-05-07 13:14:59 +0200

    Refactor HTML parsing module and re-allocate dependencies
    Migrated HTML parsing functionality from `fsharp-infrastructure` to `fsharp-web` as `Web.Clients.Browser.Html`. Removed `Infrastructure.Parser` module and cleaned up references in dependent modules. Updated `HtmlAgilityPack` to version 1.12.1 in `fsharp-web`.

---
+++ src/clients/browser/Html.fs
@@ -0,0 +1,54 @@
+﻿[<RequireQualifiedAccess>]
+module Web.Clients.Browser.Html
+
+open HtmlAgilityPack
+open Infrastructure.Domain
+open Infrastructure.Prelude
+
+let load (html: string) =
+    try
+        let document = HtmlDocument()
+        document.LoadHtml html
+        Ok document
+    with ex ->
+        Error
+        <| Operation {
+            Message = "Failed to load HTML document. " + (ex |> Exception.toMessage)
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }
+
+let getNode (xpath: string) (html: HtmlDocument) =
+    try
+        match html.DocumentNode.SelectSingleNode(xpath) with
+        | null -> Ok None
+        | node -> Ok <| Some node
+    with ex ->
+        Error
+        <| Operation {
+            Message = "Failed to select node. " + (ex |> Exception.toMessage)
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }
+
+let getNodes (xpath: string) (html: HtmlDocument) =
+    try
+        match html.DocumentNode.SelectNodes(xpath) with
+        | null -> Ok None
+        | nodes -> Ok <| Some nodes
+    with ex ->
+        Error
+        <| Operation {
+            Message = "Failed to select nodes. " + (ex |> Exception.toMessage)
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }
+
+let getAttributeValue (attribute: string) (node: HtmlNode) =
+    try
+        match node.GetAttributeValue(attribute, "") with
+        | "" -> Ok None
+        | value -> Ok <| Some value
+    with ex ->
+        Error
+        <| Operation {
+            Message = "Failed to get attribute value. " + (ex |> Exception.toMessage)
+            Code = (__SOURCE_DIRECTORY__, __SOURCE_FILE__, __LINE__) |> Line |> Some
+        }
